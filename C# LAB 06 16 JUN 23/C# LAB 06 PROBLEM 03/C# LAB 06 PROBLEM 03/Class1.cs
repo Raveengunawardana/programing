a57375f93afdb8b8098fdc825bc10192c6d7d6e1
Raveengunawardana <140948396+Raveengunawardana@users.noreply.github.com>
2023-07-31 19:24:52 +0530

    Add files via upload

---
+++ C# LAB 06 16 JUN 23/C# LAB 06 PROBLEM 03/C# LAB 06 PROBLEM 03/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__LAB_06_PROBLEM_03
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            Dog myDog = new Dog();
+            myDog.Animal();
+            myDog.Dog();
+        }
+    }
+}
---
+++ C# LAB 06 16 JUN 23/C# LAB 06 PROBLEM 03/C# LAB 06 PROBLEM 03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__LAB_06_PROBLEM_03
+{
+    internal class Animal
+    {
+        public void Animal()
+        {
+            Console.WriteLine("I am an animal");
+        }
+    }
+    class Dog : Animal
+    {
+        public void Dog()
+        {
+            Console.WriteLine("I have four legs");
+        }
+    }
+}
