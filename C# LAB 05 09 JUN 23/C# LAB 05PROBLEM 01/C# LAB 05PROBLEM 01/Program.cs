98e85276c6fbb98e79315d3633061c2a83e830a3
Raveengunawardana <140948396+Raveengunawardana@users.noreply.github.com>
2023-07-31 19:23:35 +0530

    Add files via upload

---
+++ C# LAB 05 09 JUN 23/C# LAB 05PROBLEM 01/C# LAB 05PROBLEM 01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__LAB_05PROBLEM_01
+{
+    class CalculateValues
+    {
+        public static int Addition(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int Subtraction(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public static int Multiplication(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static int Division(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
