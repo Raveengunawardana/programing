cd546df7a294f66d9062be33043abaa4db1780e3
Raveengunawardana <140948396+Raveengunawardana@users.noreply.github.com>
2023-07-31 19:22:45 +0530

    Add files via upload

---
+++ c# lab 04 02 JUN 23/C# LAB 04  PROBLEM 04/C# LAB 04  PROBLEM 04/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__LAB_04__PROBLEM_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.14159;
+            Console.WriteLine("Enter the radius of the circle: ");
+            double radius = Convert.ToDouble(Console.ReadLine());
+            double area = pi * radius * radius;
+            double circumference = 2 * pi * radius;
+            Console.WriteLine("The area of the circle is {0}.", area);
+            Console.WriteLine("The circumference of the circle is {0}.", circumference);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ c# lab 04 02 JUN 23/C# LAB 04 PROBLEM 1/C# LAB 04 PROBLEM 1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C__LAB_04_PROBLEM_1
+{
+    internal class ConvertValues
+    {
+        public void kilometerToMeter(double kilometers)
+        {
+            double meters = kilometers * 1000;
+            Console.WriteLine("{0} kilometers is equal to {1} meters.", kilometers, meters);
+            Console.ReadKey();
+        }
+    }
+}
